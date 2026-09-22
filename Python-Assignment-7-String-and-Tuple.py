39fb9067048caafddeec4efc71048fc7206c31c7
AkhilAshokGhub <145967039+AkhilAshokGhub@users.noreply.github.com>
2026-09-22 19:10:26 +0530

    Add files via upload

---
+++ Python-Assignment-7-String-and-Tuple.py
@@ -0,0 +1,15 @@
+
+string1 = "Hello "
+string2 = input("Enter your name: ")
+string3 = ", welcome to Python programming"
+result = string1 + string2 + string3
+print(result)
+
+print(result[0])
+print(result[-1])
+print(result[:5])
+print(result[-11:])
+print(result[::-1])
+print(result[23:29])
+
+
