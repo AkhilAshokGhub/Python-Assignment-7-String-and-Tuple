575eabe3ea214a397750ca361b97e3666173de19
AkhilAshokGhub <145967039+AkhilAshokGhub@users.noreply.github.com>
2026-09-23 18:54:26 +0530

    Add files via upload
    updated version

--- Python-Assignment-7-String-and-Tuple.py
+++ Python-Assignment-7-String-and-Tuple.py
@@ -1,7 +1,7 @@
 
 string1 = "Hello "
 string2 = input("Enter your name: ")
-string3 = ", welcome to Python programming"
+string3 = ", welcome to Python programming"   
 result = string1 + string2 + string3
 print(result)
 
@@ -10,6 +10,33 @@ print(result[-1])
 print(result[:5])
 print(result[-11:])
 print(result[::-1])
-print(result[23:29])
+print(result[:20])
+
+
+strM = "Python beginner tutorial"
+print(strM.upper())
+print(strM.lower())
+print(strM.capitalize()) 
+print(strM.count('t'))
+print(strM.replace("Python", "Machine Learning"))
+
+
+
+t1 = (10, 20, 30)
+t2 = (40, 50, 60)
+
+
+t_combine = t1 + t2
+print( t_combine)
+
+t_repeat = t_combine * 3
+print( t_repeat)
+
+print( t_combine[2])
+
+print( t_combine[:3])
+
+print( t_combine[-3:])
+
 
 
